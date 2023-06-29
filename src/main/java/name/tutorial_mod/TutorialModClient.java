8e28c1b9d6f517c13b52a265e3a257d2aab1d8fa
123jinhyin <1545912679@qq.com>
2023-06-29 17:21:38 +0800

    变更

---
+++ src/main/java/name/tutorial_mod/TutorialModClient.java
@@ -0,0 +1,20 @@
+package name.tutorial_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * ClassName: TutorialModClient
+ * Package: name.tutorial_mod
+ * Description:
+ *
+ * @Author: ZSTU_JY
+ * @Create: 2023/6/29 - 17:07
+ * @Version: v1.0
+ */
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
